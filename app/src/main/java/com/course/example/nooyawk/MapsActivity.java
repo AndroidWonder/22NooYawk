c92566c47586a934f8c18ca5ecfc1bb04da00603
Android Wonder <jim.pepe@verizon.net>
2021-03-17 14:51:36 -0400

    convert to Androidx libraries

--- app/src/main/java/com/course/example/nooyawk/MapsActivity.java
+++ app/src/main/java/com/course/example/nooyawk/MapsActivity.java
@@ -1,7 +1,7 @@
 package com.course.example.nooyawk;
 
 import android.content.res.Resources;
-import android.support.v4.app.FragmentActivity;
+import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
